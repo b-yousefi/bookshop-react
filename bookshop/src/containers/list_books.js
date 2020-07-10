7fb1fa67d83dd7927ede7d8702fb8bae6726ee9b
b-yousefi <b.yousefi2911@gmail.com>
2020-07-10 05:10:02 +0430

    [refactor] add price and quantity to book

--- bookshop/src/containers/list_books.js
+++ bookshop/src/containers/list_books.js
@@ -7,8 +7,15 @@ import {
     CardActionArea,
     CardMedia,
     CardContent,
+    CardActions,
     Typography,
+    IconButton,
+    Badge,
+    Box,
+    Tooltip,
 } from '@material-ui/core';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import RemoveShoppingCartIcon from '@material-ui/icons/RemoveShoppingCart';
 import {
     Link,
 } from 'react-router-dom';
@@ -23,18 +30,43 @@ class BooKList extends Component {
                     <CardActionArea component={Link}
                         to={{
                             pathname: `/books/${key}`,
-                        }} style={{ height: "100%" }}>
+                        }} >
                         <CardMedia
                             className={classes.media}
                             image={`data:image/jpeg;base64,${book.picture.data}`}
                             title={book.name}
                         />
-                        <CardContent component="div" style={{ height: "100%" }}>
-                            <Typography gutterBottom component="h5">
+
+                        <CardContent component="div" className={classes.title} title={book.name}>
+                            <Typography noWrap gutterBottom component="h5">
                                 {book.name}
                             </Typography>
                         </CardContent>
                     </CardActionArea>
+                    <CardActions disableSpacing>
+                        <Box display="flex" flexDirection="row" width="100%">
+                            <Box alignSelf="center" flexGrow={1} >
+                                <Typography gutterBottom component="h5" >
+                                    Price: {book.price}$
+                                </Typography>
+                            </Box>
+                            <Box>
+                                {/* todo: if already exist show the remove button */}
+                                <Tooltip title="Remove from shopping cart" aria-label="remove from shopping cart">
+                                <IconButton aria-label="remove from shopping cart">
+                                    <RemoveShoppingCartIcon />
+                                </IconButton>
+                                </Tooltip>
+                                <Tooltip title="Add to shopping cart" aria-label="add to shopping cart">
+                                <IconButton aria-label="add to shopping cart" disabled={book.quantity === 0}>
+                                    <Badge badgeContent={4} color="secondary">
+                                        <AddShoppingCartIcon />
+                                    </Badge>
+                                </IconButton>
+                                </Tooltip>
+                            </Box>
+                        </Box>
+                    </CardActions>
                 </Card>
             </Grid>
         );
@@ -64,12 +96,20 @@ const useStyles = theme => ({
         flexGrow: 1,
     },
     mediaroot: {
-        height: "100%"
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        height: '100%'
     },
     media: {
         height: 0,
         paddingTop: '130%',
     },
+    title: {
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        // height: /* Just enough to show 2 lines */
+    }
 });
 
 function mapStateToProps(state) {
